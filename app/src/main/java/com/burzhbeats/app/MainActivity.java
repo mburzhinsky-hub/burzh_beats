46527e32b49704fb379d275386907dfa1db423d8
mburzhinsky-hub <m.burzhinsky@visuals.ru>
2026-09-23 20:09:12 +0300

    Match reference fullscreen standby behavior

--- app/src/main/java/com/burzhbeats/app/MainActivity.java
+++ app/src/main/java/com/burzhbeats/app/MainActivity.java
@@ -8,12 +8,17 @@ import android.media.MediaPlayer;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.os.Build;
+import android.content.res.Configuration;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -80,9 +85,13 @@ public class MainActivity extends Activity {
         w.setStatusBarColor(Color.BLACK);
         w.setNavigationBarColor(Color.BLACK);
         w.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        updateSystemBars();
 
         webView = new WebView(this);
         webView.setBackgroundColor(Color.BLACK);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
@@ -93,6 +102,41 @@ public class MainActivity extends Activity {
         webView.loadUrl("file:///android_asset/index.html");
         main.postDelayed(this::loadPlaylist, 500);
         main.post(progressTicker);
+    }
+
+    private void updateSystemBars() {
+        boolean landscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        Window w = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController controller = w.getInsetsController();
+            if (controller != null) {
+                if (landscape) {
+                    controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                    controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                } else {
+                    controller.show(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                }
+            }
+        } else {
+            View decor = w.getDecorView();
+            if (landscape) {
+                decor.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            } else {
+                decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateSystemBars();
     }
 
     private void loadPlaylist() {
